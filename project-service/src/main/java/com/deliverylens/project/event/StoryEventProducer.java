2b996db98cfb359c5698215c0e037c1dfa3652b2
Kriti Atrishi <kritiatrishi@gmail.com>
2026-09-13 23:26:16 +0530

    Added more Event-Mapping related StoryService Patch mapping changed

--- project-service/src/main/java/com/deliverylens/project/event/StoryEventProducer.java
+++ project-service/src/main/java/com/deliverylens/project/event/StoryEventProducer.java
@@ -2,7 +2,9 @@ package com.deliverylens.project.event;
 
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+@Service
 public class StoryEventProducer {
+
     private static final String TOPIC= "story-events";
 
     private final KafkaTemplate<String,StoryStatusChangedEvent> kafkaTemplate;
--- project-service/src/main/java/com/deliverylens/project/service/StoryService.java
+++ project-service/src/main/java/com/deliverylens/project/service/StoryService.java
@@ -2,6 +2,8 @@ package com.deliverylens.project.service;
 
 import com.deliverylens.project.entity.Status;
 import com.deliverylens.project.entity.Story;
+import com.deliverylens.project.event.StoryEventProducer;
+import com.deliverylens.project.event.StoryStatusChangedEvent;
 import com.deliverylens.project.repository.StoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StoryService {
     private final StoryRepository repository;
+
+    //    @Autowired
+    //in case it is not constructor injection
+    private final StoryEventProducer storyEventProducer;
 
     public Story createStory(Story story) {
 
@@ -26,8 +32,14 @@ public class StoryService {
     }
 
     public Story updateStatus(Long id, Status status) {
-        Story story = repository.findById(id).orElseThrow(()-> new RuntimeException("Story Not Found"));
+
+        Story story = repository.findById(id).orElseThrow(() -> new RuntimeException("Story Not Found"));
+        Status oldStatus=story.getStatus();
         story.setStatus(status);
-        return repository.save(story);
+        Story updatedStory=repository.save(story);
+        StoryStatusChangedEvent event=new StoryStatusChangedEvent(updatedStory.getId(), updatedStory.getSprintId(),oldStatus!=null? oldStatus.name():null,updatedStory.getStatus().name());
+        storyEventProducer.publishStatusChanged(event);
+        return updatedStory;
+//        return repository.save(story);
     }
 }
